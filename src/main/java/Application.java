0a9b2346f3b2a9c0ee283fd18455d8db47ca4073
Glebasta33 <gleb.trusow@yandex.ru>
2021-08-10 16:04:10 +0300

    simple onUpadeteReceived method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,3 @@
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,5 +1,8 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -12,12 +15,37 @@ public class Bot extends TelegramLongPollingBot {
     // токен от BotFather
     @Override
     public String getBotToken() {
-        return null;
+        return TokenStorage.API_TOKEN;
     }
 
     // обновление получаемых сообщений
     @Override
     public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help":
+                    sendMsg(message, "Чем могу помочь ?");
+                    break;
+                case "/settings":
+                    sendMsg(message, "Что нужно настроить ?");
+                    break;
+                default:
+            }
+        }
+    }
 
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true); // for many variables answers
+        sendMessage.setChatId(message.getChatId().toString()); // для получения id чата
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
